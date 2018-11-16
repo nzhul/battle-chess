98ad2c87b4307071bc166fdf1e6ba27fe7a53a81
nzhul <dobromirivanov1@gmail.com>
2018-11-16 09:51:14 +0200

    add empty unity project and starting work on boardmanager

---
+++ BattleChess/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    private const float TILE_SIZE = 1f;
+    private const float TILE_OFFSET = .5f;
+
+    private int selectionX = -1; // TODO: use Coord struct instead.
+    private int selectionY = -1;
+
+    public List<GameObject> piecePrefabs;
+    private List<GameObject> activePieces; // TODO: change this with Runtime sets ( scriptable objects )
+
+    public LayerMask layerMask;
+
+    private void Update()
+    {
+        UpdateSelection();
+        DrawBoard();
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25f, layerMask))
+        {
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+
+    private void DrawBoard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for (int j = 0; j <= 8; j++)
+            {
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + heightLine);
+            }
+        }
+
+        // Draw the selection
+        if (selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(Vector3.forward * selectionY + Vector3.right * selectionX, Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
+
+            Debug.DrawLine(Vector3.forward * (selectionY + 1) + Vector3.right * selectionX, Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
+        }
+    }
+}
